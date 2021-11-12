126004516dc6c79acdb78a58d715552340ebe962
Andre <36155981+Neaxic@users.noreply.github.com>
2021-11-12 16:10:59 +0100

    up

--- Kontakt.php
+++ Kontakt.php
@@ -170,13 +170,11 @@ $message_sent = false;
 
         </div> <!-- /.container-->
 </body>
-<div class="row">
-    <div class="col-12" style="margin-top: 100px;" height="450">
-        <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1353.9249621276626!2d8.843507638492952!3d56.805394908207944!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x464977e92cfd0cd1%3A0xdc260f11c78364c3!2sIslandsvej%2C%207900%20Nyk%C3%B8bing%20Mors!5e1!3m2!1sen!2sdk!4v1578847182562!5m2!1sen!2sdk"
-            width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+<div class="row" style="margin-top: 7px;">
+      <div class="col-12">
+        <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1d1MWmrr4e8K92iCGfdes6Va1yC2eOR9T" width="100%" height="450"></iframe>    
+      </div>
     </div>
-</div>
 <div
     style="background-color: rgb(61, 61, 61); text-decoration:none; color:white; text-align: center; padding-top: 25px;">
     <div class="row col-12 justify-content-center">
